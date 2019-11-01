e11472554473eda4b94bbc0178a2dbb70c1d0218
Dominykas Kutka <dominykaskulka@yahoo.com>
2019-11-01 16:00:47 +0100

    small step

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 "use strict";
+
 var audio = new Audio("DuckTales Music (NES) - The Moon Theme.mp3");
+const assignmentName = document
+  .querySelector("body > svg > text")
+  .textContent.trim();
+
+let i = 0;
+let delay = 50;
+document.querySelector("body > svg > text").textContent = "";
 
 document
   .querySelector("#button > circle")
@@ -11,5 +19,26 @@ function startIntro() {
     document.querySelector("body").style.backgroundColor = "black";
     document.querySelector("#button > circle").style.display = "none";
     audio.play();
+
+    setTimeout(() => {
+      typeName();
+    }, 2800);
   }, 800);
+}
+
+function typeName() {
+  if (i < assignmentName.length) {
+    document.querySelector("body > svg > text").textContent +=
+      assignmentName[i];
+    i++;
+    setTimeout(typeName, delay + Math.floor(Math.random() * 20));
+  } else {
+    setTimeout(() => {
+      explainName();
+    }, 2000);
+  }
+}
+
+function explainName() {
+  document.querySelector("body > svg > text").textContent = "type";
 }
