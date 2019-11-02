7236f9ee4593eea102d9e148534cce0ad8f81295
Dominykas Kutka <dominykaskulka@yahoo.com>
2019-11-02 01:07:43 +0100

    added more animations

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+console.log(document.querySelector("#square > rect").width.baseVal.value);
+
 var audio = new Audio("DuckTales Music (NES) - The Moon Theme.mp3");
 const assignmentName = document
   .querySelector("body > svg > text")
@@ -19,6 +21,7 @@ function startIntro() {
     document.querySelector("body").style.backgroundColor = "black";
     document.querySelector("#button > circle").style.display = "none";
     audio.play();
+    audio.volume = 0.1;
 
     setTimeout(() => {
       typeName();
@@ -34,11 +37,63 @@ function typeName() {
     setTimeout(typeName, delay + Math.floor(Math.random() * 20));
   } else {
     setTimeout(() => {
-      explainName();
+      deleteText(document.querySelector("body > svg > text").textContent);
     }, 2000);
   }
 }
 
-function explainName() {
-  document.querySelector("body > svg > text").textContent = "type";
+function deleteText(input) {
+  if (input.length > 0) {
+    document.querySelector("body > svg > text").textContent = input.substring(
+      0,
+      input.length - 1
+    );
+    setTimeout(() => {
+      deleteText(document.querySelector("body > svg > text").textContent);
+    }, delay + Math.floor(Math.random() * 20));
+  } else {
+    i = 0;
+    typeRealName();
+  }
+}
+
+function typeRealName() {
+  var text = `"The Intro"`;
+  if (i < text.length) {
+    document.querySelector("body > svg > text").textContent += text[i];
+    i++;
+    setTimeout(typeRealName, delay + Math.floor(Math.random() * 20));
+  } else {
+    setTimeout(() => {
+      deleteRealName(document.querySelector("body > svg > text").textContent);
+    }, 2000);
+  }
+}
+
+function deleteRealName(input) {
+  if (input.length > 0) {
+    document.querySelector("body > svg > text").textContent = input.substring(
+      0,
+      input.length - 1
+    );
+    setTimeout(() => {
+      deleteRealName(document.querySelector("body > svg > text").textContent);
+    }, delay + Math.floor(Math.random() * 20));
+  } else {
+    i = 0;
+    typeGroupName();
+  }
+}
+
+function typeGroupName() {
+  var text = `By GroupName`;
+  if (i < text.length) {
+    document.querySelector("body > svg > text").textContent += text[i];
+    i++;
+    setTimeout(typeGroupName, delay + Math.floor(Math.random() * 20));
+  } else {
+    setTimeout(() => {
+      document.querySelector("#square > rect").width.baseVal.value = 0;
+    }, 2000);
+  }
 }
